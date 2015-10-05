bed1d8cac81d0f937db43c6449b122de7577a38b
radiegtya <radiegtya@yahoo.co.id>
2015-10-05 14:35:20 +0700

    batch input dipa with validation done

--- protected/extensions/appendo/views/package.php
+++ protected/extensions/appendo/views/package.php
@@ -10,7 +10,7 @@
     <tbody>
         <?php if ($model->code == null): ?>
             <tr>
-                <td><?php echo CHtml::dropDownList('code[]', "string", Subcomponent::model()->getNonUsedOnPackage(), array('prompt' => 'Pilih','class'=>'code')); ?> </td>
+                <td><?php echo CHtml::dropDownList('code[]', "string", Subcomponent::model()->getNonUsedOnPackage(), array('prompt' => 'Pilih', 'class' => 'code', 'type'=>'selc', 'onfocus'=>'removeDuplicate()')); ?> </td>
                 <td><?php echo CHtml::dropDownList('ppk_code[]', "string", Ppk::model()->getPpkOptions(), array('prompt' => 'Pilih')); ?> </td>
                 <td><?php echo CHtml::dropDownList('province_code[]', "string", Province::model()->getOptionsCodeName(), array('prompt' => 'Pilih')); ?> </td>
                 <td><?php echo CHtml::dropDownList('city_code[]', "string", City::model()->getOptionsCodeName(), array('prompt' => 'Pilih')); ?> </td>
@@ -18,14 +18,14 @@
         <?php else: ?>
             <?php for ($i = 0; $i < sizeof($model->code); ++$i): ?>
                 <tr>
-                    <td><?php echo CHtml::dropDownList('code[]', "string", Subcomponent::model()->getSubcomponentOptions(), array('prompt' => 'Pilih')); ?> </td>
+                    <td><?php echo CHtml::dropDownList('code[]', "string", Subcomponent::model()->getSubcomponentOptions(), array('prompt' => 'Pilih', 'type'=>'selc', 'onfocus'=>'removeDuplicate()')); ?> </td>
                     <td><?php echo CHtml::dropDownList('ppk_code[]', "string", Ppk::model()->getPpkOptions(), array('prompt' => 'Pilih')); ?> </td>
                     <td><?php echo CHtml::dropDownList('province_code[]', "string", Province::model()->getOptionsCodeName(), array('prompt' => 'Pilih')); ?> </td>
                     <td><?php echo CHtml::dropDownList('city_code[]', "string", City::model()->getOptionsCodeName(), array('prompt' => 'Pilih')); ?> </td>
                 </tr>
             <?php endfor; ?>
             <tr>
-                <td><?php echo CHtml::dropDownList('code[]', "string", Subcomponent::model()->getSubcomponentOptions(), array('prompt' => 'Pilih')); ?> </td>
+                <td><?php echo CHtml::dropDownList('code[]', "string", Subcomponent::model()->getSubcomponentOptions(), array('prompt' => 'Pilih', 'type'=>'selc', 'onfocus'=>'removeDuplicate()')); ?> </td>
                 <td><?php echo CHtml::dropDownList('ppk_code[]', "string", Ppk::model()->getPpkOptions(), array('prompt' => 'Pilih')); ?> </td>
                 <td><?php echo CHtml::dropDownList('province_code[]', "string", Province::model()->getOptionsCodeName(), array('prompt' => 'Pilih')); ?> </td>
                 <td><?php echo CHtml::dropDownList('city_code[]', "string", City::model()->getOptionsCodeName(), array('prompt' => 'Pilih')); ?> </td>
@@ -33,3 +33,26 @@
         <?php endif; ?>
     </tbody>
 </table>
+
+<script>
+    var removeDuplicate = function() {
+        $("select[type='selc']").find("option").each(function() {
+            var val = $(this).val();
+            var flag = false;
+
+            if (!$(this).attr('selected')) {
+                $("select[type='selc']").find("option:selected").each(function() {
+                    if ($(this).val() == val) {
+                        flag = true;
+                        return;
+                    }
+                });
+            }
+
+            if (flag)
+                $(this).hide();
+            else
+                $(this).show();
+        })
+    }
+</script>
